7b2c7403ca54adde081a059af0521cd2918ac872
Alexander Popov <alexander.popov.gg@gmail.com>
2020-06-15 13:56:34 +0300

    entity model page method

--- src/Models/EntityModel.php
+++ src/Models/EntityModel.php
@@ -7,10 +7,23 @@ use Illuminate\Support\Arr;
 
 abstract class EntityModel extends Model
 {
+    public $rules = [];
+
     public function __construct(array $attributes = [])
     {
         $this->with = array_merge((array)$this->with, ['translation']);
         parent::__construct($attributes);
+    }
+
+    public static function page(array $page, array $with = [])
+    {
+        $request = app('request');
+        $query = static::query();
+        if (count($with)) {
+            $query = $query->with($with);
+        }
+        return $query->paginate($page['size'], ['*'], 'page[number]', $page['number'])
+            ->withPath($request->fullUrlWithQuery($request->except('page.number')));
     }
 
     /**
